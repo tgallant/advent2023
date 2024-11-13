2c79f0b0522a3ef97a2b3ca71ccf271ab2e08fb8
Tim Gallant <tim@lilt.com>
2024-11-13 14:42:20 -0800

    adds day 4 pt 2

--- lisp/04.el
+++ lisp/04.el
@@ -34,10 +34,28 @@
         (mapcar 'score-winnings)
         (apply '+)))
 
+(defun score-winnings-v2 (winnings)
+  (defun depth-count (lst)
+    (cond ((eq lst nil) 0)
+          (t (+ (count-card 0 (car lst)) (depth-count (cdr lst))))))
+  (defun count-card (acc cur)
+    (+ acc 1 (depth-count (number-sequence (+ 1 cur) (+ cur (nth cur winnings))))))
+  (cl-reduce 'count-card
+             (number-sequence 0 (- (length winnings) 1))
+             :initial-value 0))
+
+(defun 2023-04-part2 (input)
+  (-<>> (mapcar 'make-card input)
+        (mapcar 'determine-winnings)
+        (mapcar 'length)
+        (score-winnings-v2)))
+
 (defconst testfile (expand-file-name "input/04.test.txt"))
 (defconst inputfile (expand-file-name "input/04.input.txt"))
 
 (defcheck 2023-04-part1 testfile 13)
 (defcheck 2023-04-part1 inputfile 18653)
+(defcheck 2023-04-part2 testfile 30)
+(defcheck 2023-04-part2 inputfile 5921508)
 
 (solve "2023-04")
