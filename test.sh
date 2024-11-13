5ae67bb8f5d3128d2b95d83d881d86f90f1df3b4
Tim Gallant <tim@lilt.com>
2024-11-12 21:56:53 -0800

    fix tests

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd "lisp"
-emacs -batch -l aoc2023.el -l $1.el -f ert-run-tests-batch-and-exit
+emacs -batch -l aoc2023.el -l $1 -f ert-run-tests-batch-and-exit
 STATUS=$?
 cd -
 exit $STATUS
