b1186b6c66974538eb81d120967192a5a90eb14c
Tim Gallant <tim@lilt.com>
2024-11-23 14:41:40 -0800

    adds day 6 part 2

--- lisp/06.el
+++ lisp/06.el
@@ -8,10 +8,22 @@
         (string-split <> " " t)
         (mapcar 'string-to-number)))
 
+(defun make-number-from-list (str)
+  (-<>> (string-split str ":" t)
+        (nth 1)
+        (string-split <> " " t)
+        (string-join)
+        (string-to-number)))
+
 (defun make-races (lst)
   (cl-mapcar 'cons
              (make-number-list (nth 0 lst))
              (make-number-list (nth 1 lst))))
+
+(defun make-races-v2 (lst)
+  (cons
+   (make-number-from-list (nth 0 lst))
+   (make-number-from-list (nth 1 lst))))
 
 (defun score-race (race)
   (cl-destructuring-bind (distance . record) race
@@ -27,10 +39,16 @@
         (mapcar 'score-race)
         (apply '*)))
 
+(defun 2023-06-part2 (input)
+  (-<>> (make-races-v2 input)
+        (score-race)))
+
 (defconst testfile (expand-file-name "input/06.test.txt"))
 (defconst inputfile (expand-file-name "input/06.input.txt"))
 
 (defcheck 2023-06-part1 testfile 288)
 (defcheck 2023-06-part1 inputfile 633080)
+(defcheck 2023-06-part2 testfile 71503)
+(defcheck 2023-06-part2 inputfile 20048741)
 
 (solve "2023-06")
